980707e4f9063cbaebc5dba9b0ac507224fead0a
sangmin park <psm9417@gmail.com>
2018-12-15 02:27:39 +0900

    Add : button-icon

--- App.js
+++ App.js
@@ -1,34 +1,39 @@
 import React from 'react';
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import Foundation from 'react-native-vector-icons/Foundation'
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
 import HomeScreen from './Screens/HomeScreen';
 import HospitalScreen from './Screens/Hospital';
 import SettingsScreen from './Screens/Settings';
 const TabNavigator = createBottomTabNavigator(
   {
-    Home: HomeScreen,
-    Hospital: HospitalScreen,
-    Settings: SettingsScreen,
+    진찰: HomeScreen,
+    병원매칭: HospitalScreen,
+    // 세팅: SettingsScreen,
   },
   {
     defaultNavigationOptions: ({ navigation }) => ({
       tabBarIcon: ({ focused, horizontal, tintColor }) => {
-        const { routeName } = navigation.state;
+        const routeName = navigation.state.routeName;
+        console.log( routeName )
         let iconName;
-        if (routeName === 'Home'){
-          iconName = `ios-home`;
-        } else if (routeName === 'Hospital'){
-          iconName = 'ios-medkit';
-        } else if (routeName === 'Settings'){
-          iconName = `ios-settings`;
-        }
+        if (routeName === '진찰'){
+          iconName = `stethoscope`;
+          return <FontAwesome name = {iconName} size={horizontal ? 20 : 25} color={tintColor} />;
+        } else if (routeName === '병원매칭'){
+          iconName = 'map';
+          return <Foundation name = {iconName} size={horizontal ? 20 : 25} color={tintColor} />
+        } 
+        // else if (routeName === '세팅'){
+        //   iconName = `ios-settings`;
+        //   return <Ionicons name = {iconName} size={horizontal ? 20 : 25} color={tintColor} />;
+        // }
         // You can return any component that you like here! We usually use an
         // icon component from react-native-vector-icons
-        return <Ionicons name={iconName} size={horizontal ? 20 : 25} color={tintColor} />;
       },
     }),
     tabBarOptions: {
-      activeTintColor: '#00695C',
+      activeTintColor: '#44dd33',
       inactiveTintColor: 'gray',
     },
   }
--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,14 +1,80 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { Text, View, TouchableHighlight, StyleSheet } from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import { Camera, Permissions } from 'expo';
 
 class HomeScreen extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            buttons:[
+                {
+                    Icon: "run",
+                    Text: "보행분석"
+                },
+                {
+                    Icon: "human",
+                    Text: "벨런스분석"
+                }
+            ],
+        }
+    }
+
+    async componentWillMount() {
+
+    }
+
     render() {
         return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>Home!</Text>
+            <View style = {styles.contents}>
+                <TouchableHighlight
+                        underlayColor="#eeeeee"
+                        onPress={() => {this._onButtonPress(this.state.buttons[0].Text)}}>
+                        <View style = {styles.button}>
+                            <MaterialCommunityIcons name = {this.state.buttons[0].Icon} size = {80}/>
+                            <Text style = {styles.text}>{this.state.buttons[0].Text}</Text>
+                        </View>
+                </TouchableHighlight>
+
+                <TouchableHighlight
+                        underlayColor="#eeeeee"
+                        onPress={() => {this._onButtonPress(this.state.buttons[1].Text)}}>
+                        <View style = {styles.button}>
+                            <MaterialCommunityIcons name = {this.state.buttons[1].Icon} size = {80}/>
+                            <Text style = {styles.text}>{this.state.buttons[1].Text}</Text>
+                        </View>
+                </TouchableHighlight>
             </View>
         );
     }
+    _onButtonPress = (m) => {
+        console.log(m)
+    }
 }
 
+const styles = StyleSheet.create({
+    contents: {
+        flex: 1, 
+        justifyContent: 'center', 
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+    },
+    button: {
+        backgroundColor:'#77aa66',
+        borderRadius: 15,
+        borderWidth: 1,
+        padding: 2,
+        borderColor: 'white',
+        justifyContent: 'center', 
+        textAlignVertical: "center",
+        textAlign: "center"
+    },
+    text: {
+        textAlignVertical: "center",
+        textAlign: "center"
+    }
+});
+
+  
 export default HomeScreen;
